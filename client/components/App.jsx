23a3838b551e150a17c7415d8c899aebc2ea07ae
Danny <Danny@Apples-iMac.local>
2021-11-04 16:28:35 +1300

    web app built

---
+++ client/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+
+function App () {
+  return (
+    <>
+      <div>
+        <h1>The Clothing Safari!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
