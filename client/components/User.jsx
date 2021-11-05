7ac1dfd801811e5cbb76d6c39434de2d628baf3a
Danny <Danny@Apples-iMac.local>
2021-11-05 14:35:43 +1300

    almost there

--- client/components/User.jsx
+++ client/components/User.jsx
@@ -10,31 +10,20 @@ function User (props) {
   const quote = users.quote
   return (
     <>
-<<<<<<< HEAD
     <div className="container-user">
       <Nav user={name}/>
       <div className="user-text">
         <h2>Hi I'm {name}! Take me on a clothing Safari around the world!</h2>
-        <p>{quote}</p>
       </div>
       <div className="user-image">
         <img src={`/images/${userImage}`}></img>
+        <img src={`/images/${quote}`}></img>
       </div>
     </div>
-||||||| merged common ancestors
-    <Nav user={name}/>
-    <h2>Hi I'm {name}! Take me on a clothing Safari around the world!</h2>
-    <p>{quote}</p>
-    <img src={`/images/${userImage}`}></img>
-
-=======
-    <Nav user={name}/>
-    <h2>Hi I'm {name}! Take me on a clothing Safari around the world!</h2>
-    <img src={`/images/${userImage}`}></img>
-    <img src={`/images/${quote}`}></img>
+    
+    
     
 
->>>>>>> main
     </>
   )
 }
